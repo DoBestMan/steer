fa5a1fa704d521abf1b15d74200ca90e0d00f2dc
Arnaud Tanielian <arno.tanielian@gmail.com>
2020-04-07 22:36:53 -0400

    fix(storybook): updated global styles path

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,7 @@
 import { addDecorator } from '@storybook/react';
 
 import { Global } from '@emotion/core';
-import { global } from '../src/styles/global.styles';
+import { global } from '../src/styles/global/global.styles';
 
 const withGlobal = (cb) => (
   <>
