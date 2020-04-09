d2d5fff3ec7c8a552ac837619d95c3f644e5b3a2
Alex Van Holtz <vanholtz@work.co>
2020-04-09 10:00:07 -0700

    style(link): change padding to int and add spacing

--- src/components/global/Link/Link.styles.ts
+++ src/components/global/Link/Link.styles.ts
@@ -2,7 +2,8 @@ import { css } from '@emotion/core';
 
 export const link = css({
   '&:hover,&:focus': { borderColor: 'inherit' },
+
   borderBottom: '2px dotted transparent',
-  paddingBottom: '2',
+  paddingBottom: 2,
   transition: 'border-color 100ms ease',
 });
