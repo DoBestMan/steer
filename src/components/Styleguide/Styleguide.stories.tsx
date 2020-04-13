51ddd1e4e5236d8facc111d9cf15974dc67ebb5c
Sarah Mogin <mogin@work.co>
2020-04-13 14:32:54 -0400

    chore: move styleguide content to stories (#22)

--- src/components/Styleguide/Styleguide.stories.tsx
+++ src/components/Styleguide/Styleguide.stories.tsx
@@ -1,4 +1,9 @@
+import { styles } from './styleguide.styles';
+
 import { typography } from '~/styles/global/typography.styles';
+
+import Grid from '~/components/global/Grid/Grid';
+import GridItem from '~/components/global/Grid/GridItem';
 
 export default {
   title: 'Styleguide',
@@ -6,15 +11,24 @@ export default {
 
 export function Text() {
   return (
-    <div>
-      <p css={typography.jumboHeadline}>Jumbo Headline</p>
-      <p css={typography.primaryHeadline}>Primary Headline</p>
-      <p css={typography.secondaryHeadline}>Secondary Headline</p>
-      <p css={typography.tertiaryHeadline}>Tertiary Headline</p>
-      <p css={typography.subhead}>Subhead</p>
-      <p css={typography.eyebrow}>Eyebrow</p>
-      <p css={typography.bodyCopy}>Body Copy</p>
-      <p css={typography.smallCopy}>Small Copy</p>
-    </div>
+    <Grid as="section">
+      <GridItem css={styles.containerText}>
+        <p css={typography.jumboHeadline}>Jumbo Headline</p>
+        <p css={typography.primaryHeadline}>Primary Headline</p>
+        <p css={typography.secondaryHeadline}>Secondary Headline</p>
+        <p css={typography.tertiaryHeadline}>Tertiary Headline</p>
+        <p css={typography.subhead}>Subhead</p>
+        <p css={typography.eyebrow}>Eyebrow</p>
+        <p css={typography.bodyCopy}>Body Copy</p>
+        <p css={typography.smallCopy}>Small Copy</p>
+      </GridItem>
+      <GridItem
+        fullbleed
+        css={[styles.containerFullbleed, typography.bodyCopy]}
+        as="p"
+      >
+        Full-bleed item
+      </GridItem>
+    </Grid>
   );
 }
