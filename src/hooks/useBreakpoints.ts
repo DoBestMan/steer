7d1dcc293509a8107bc4d76fd15bcdf2354409e9
Arnaud Tanielian <arno.tanielian@gmail.com>
2020-05-08 13:43:31 -0400

    fix(useBreakpoint): use `BREAKPOINT_SIZES.S` as default to avoid rendering issues with SSR (#142)

--- src/hooks/useBreakpoints.ts
+++ src/hooks/useBreakpoints.ts
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 
 import { useWindowSize } from '~/hooks/useWindowSize';
-import { Breakpoint } from '~/lib/constants';
+import { Breakpoint, BREAKPOINT_SIZES } from '~/lib/constants';
 import { getBreakpoint } from '~/lib/utils/breakpoints';
 
 export function useBreakpoints(): Breakpoint {
   const windowSize = useWindowSize();
-  const [bk, setBK] = useState<Breakpoint>(getBreakpoint(windowSize.width));
+  const [bk, setBK] = useState<Breakpoint>(BREAKPOINT_SIZES.S);
 
   useEffect(() => {
     setBK(getBreakpoint(windowSize.width));
