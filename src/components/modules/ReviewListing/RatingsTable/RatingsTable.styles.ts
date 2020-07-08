571f1e077d4e60e232a1583dda648f8857b75cbc
Alex Van Holtz <34043823+vanholtzatwork@users.noreply.github.com>
2020-07-08 08:12:10 -0700

    [WCS-640] RatingsTable text style updates (#546)

--- src/components/modules/ReviewListing/RatingsTable/RatingsTable.styles.ts
+++ src/components/modules/ReviewListing/RatingsTable/RatingsTable.styles.ts
@@ -1,47 +1,63 @@
 import { COLORS, MQ, SPACING, StylesMap } from '~/lib/constants';
-import { borderBottomWithGap } from '~/styles/borders.styles';
+import { borderTopWithGap } from '~/styles/borders.styles';
 import { typography } from '~/styles/typography.styles';
 
 const styles: StylesMap = {
+  bordered: [
+    borderTopWithGap,
+    {
+      // No border into gutter for the last column
+      ':last-of-type': {
+        marginRight: 0,
+      },
+    },
+  ],
   buttonContainer: {
     textAlign: 'center',
   },
-  column: [
-    borderBottomWithGap,
+  column: {
+    padding: `${SPACING.SIZE_20}px 0`,
+  },
+  container: [
+    typography.bodyCopy,
     {
-      padding: `${SPACING.SIZE_20}px 0`,
+      paddingBottom: `${SPACING.SIZE_40}px`,
+      textAlign: 'left',
+      width: '100%',
+      [MQ.S]: {
+        paddingTop: SPACING.SIZE_20,
+      },
+      [MQ.L]: {
+        paddingTop: SPACING.SIZE_60,
+      },
     },
   ],
-  container: {
-    paddingBottom: `${SPACING.SIZE_40}px`,
-    textAlign: 'left',
-    width: '100%',
-    [MQ.S]: {
-      paddingTop: SPACING.SIZE_20,
-    },
-    [MQ.L]: {
-      paddingTop: SPACING.SIZE_60,
-    },
-  },
   headingText: typography.labelHeadline,
   lastColumn: {
     marginRight: 0,
     textAlign: 'right',
   },
+  link: {
+    display: 'block',
+    '&:after': {
+      bottom: 0,
+      content: '""',
+      left: 0,
+      position: 'absolute',
+      right: 0,
+      top: 0,
+    },
+  },
   row: {
-    ':last-child': {
+    color: COLORS.LIGHT.GRAY_70,
+    position: 'relative',
+    ':hover': {
+      color: COLORS.GLOBAL.BLACK,
       td: {
-        border: 'none',
+        borderColor: COLORS.GLOBAL.BLACK,
       },
     },
-    opacity: 1,
   },
-  text: [
-    typography.bodyCopy,
-    {
-      color: COLORS.LIGHT.GRAY_70,
-    },
-  ],
 };
 
 export default styles;
--- src/components/modules/ReviewListing/RatingsTable/RatingsTable.tsx
+++ src/components/modules/ReviewListing/RatingsTable/RatingsTable.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Button from '~/components/global/Button/Button';
 import Grid from '~/components/global/Grid/Grid';
 import GridItem from '~/components/global/Grid/GridItem';
+import BaseLink from '~/components/global/Link/BaseLink';
 import Stars from '~/components/global/Stars/Stars';
 import { RATINGS } from '~/lib/constants';
 import { ui } from '~/lib/utils/ui-dictionary';
@@ -33,7 +34,7 @@ function Results({ reviews }: Props) {
 
   const hasMoreReviews = reviews.length > numVisibleReviews;
 
-  function handleButtonClick() {
+  function handleSeeMoreClick() {
     setNumVisibleReviews((prev) => (prev += CONSTANTS.MAX_REVIEWS_TO_DISPLAY));
   }
 
@@ -41,41 +42,48 @@ function Results({ reviews }: Props) {
     <>
       <RatingsTableSort resultsCount={reviews.length} />
       <table css={styles.container}>
-        <thead css={styles.headingText}>
-          <Grid as="tr">
+        <Grid as="thead" css={styles.headingText}>
+          <GridItem
+            gridColumnS="2/6"
+            gridColumnM="2/8"
+            gridColumnL="3/13"
+            gridColumnXL="4/12"
+            isGrid
+            as="tr"
+          >
             <GridItem
               as="th"
               css={styles.column}
-              gridColumnS="2/4"
-              gridColumnM="2/5"
-              gridColumnL="3/9"
-              gridColumnXL="4/9"
+              gridColumnS="1/3"
+              gridColumnM="1/4"
+              gridColumnL="1/7"
+              gridColumnXL="1/6"
             >
               {ui('tireReviews.tireHeader')}
             </GridItem>
             <GridItem
               as="th"
               css={styles.column}
-              gridColumnS="4/5"
-              gridColumnM="5/6"
-              gridColumnL="9/11"
-              gridColumnXL="9/10"
+              gridColumnS="3/4"
+              gridColumnM="4/6"
+              gridColumnL="7/9"
+              gridColumnXL="6/8"
             >
               {ui('tireReviews.ratingHeader')}
             </GridItem>
             <GridItem
               as="th"
-              gridColumnS="5/6"
-              gridColumnM="6/8"
-              gridColumnL="11/13"
-              gridColumnXL="10/12"
+              gridColumnS="4/5"
+              gridColumnM="6/7"
+              gridColumnL="9/11"
+              gridColumnXL="8/9"
               css={[styles.column, styles.lastColumn]}
             >
               {ui('tireReviews.numRatingsHeader')}
             </GridItem>
-          </Grid>
-        </thead>
-        <tbody>
+          </GridItem>
+        </Grid>
+        <Grid as="tbody">
           {reviews.slice(0, numVisibleReviews).map((review) => {
             const a11yLabel = ui('ratings.fullRating', {
               rating: review.rating,
@@ -83,24 +91,35 @@ function Results({ reviews }: Props) {
             });
 
             return (
-              <Grid as="tr" css={styles.row} key={review.id}>
+              <GridItem
+                isGrid
+                gridColumnS="2/6"
+                gridColumnM="2/8"
+                gridColumnL="3/13"
+                gridColumnXL="4/12"
+                as="tr"
+                css={styles.row}
+                key={review.id}
+              >
                 <GridItem
                   as="td"
-                  css={[styles.column, styles.headingText]}
-                  gridColumnS="2/4"
-                  gridColumnM="2/5"
-                  gridColumnL="3/9"
-                  gridColumnXL="4/9"
+                  css={[styles.column, styles.bordered, styles.headingText]}
+                  gridColumnS="1/3"
+                  gridColumnM="1/4"
+                  gridColumnL="1/7"
+                  gridColumnXL="1/6"
                 >
-                  {review.tire}
+                  <BaseLink css={styles.link} href="/">
+                    {review.tire}
+                  </BaseLink>
                 </GridItem>
                 <GridItem
                   as="td"
-                  css={styles.column}
-                  gridColumnS="4/5"
-                  gridColumnM="5/6"
-                  gridColumnL="9/11"
-                  gridColumnXL="9/10"
+                  css={[styles.column, styles.bordered]}
+                  gridColumnS="3/4"
+                  gridColumnM="4/6"
+                  gridColumnL="7/9"
+                  gridColumnXL="6/8"
                 >
                   <span>
                     <Stars isSmall number={review.rating} />
@@ -109,23 +128,24 @@ function Results({ reviews }: Props) {
                 </GridItem>
                 <GridItem
                   as="td"
-                  gridColumnS="5/6"
-                  gridColumnM="6/8"
-                  gridColumnL="11/13"
-                  gridColumnXL="10/12"
-                  css={[styles.text, styles.column, styles.lastColumn]}
+                  css={[styles.column, styles.bordered, styles.lastColumn]}
+                  gridColumnS="4/5"
+                  gridColumnM="6/7"
+                  gridColumnL="9/11"
+                  gridColumnXL="8/9"
                 >
                   {review.ratingsQuantity}
                   <span css={screenReaderText}>{ui('ratings.ratings')}</span>
                 </GridItem>
-              </Grid>
+              </GridItem>
             );
           })}
-        </tbody>
+        </Grid>
       </table>
+
       {hasMoreReviews && (
         <div css={styles.buttonContainer}>
-          <Button onClick={handleButtonClick}>{ui('tireReviews.more')}</Button>
+          <Button onClick={handleSeeMoreClick}>{ui('tireReviews.more')}</Button>
         </div>
       )}
     </>
