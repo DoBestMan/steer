6a8dcaf95f8446f6d45929e45ea276901e364caa
Alex Van Holtz <34043823+vanholtzatwork@users.noreply.github.com>
2020-07-16 12:17:56 -0700

    [WCS-1003] Create temporary breadcrumbs for reviews section (#623)

--- src/components/pages/ReviewListingPage/mappers/header.ts
+++ src/components/pages/ReviewListingPage/mappers/header.ts
@@ -12,6 +12,7 @@ import { SiteProductReviewsListingInfo } from '~/data/models/SiteProductReviewsL
 import { ROUTE_MAP, ROUTES } from '~/lib/constants';
 import { mapPathnameToBreadcrumbs } from '~/lib/utils/breadcrumbs';
 import { interpolateRoute } from '~/lib/utils/routes';
+import { ui } from '~/lib/utils/ui-dictionary';
 
 export function mapDataToHeader({
   siteInfo: { title, description },
@@ -87,18 +88,48 @@ export function mapDataToHeader({
 
   const breadcrumbLabels: Record<string, string> = {};
 
+  // Bring this back when the SEO landing pages for types/[type] and categories[category] are created
   // Need to remove tire from active filter to build [type] and [category] for breadcrumb labels
+  // Object.keys(activeFilters).forEach((key) => {
+  // This util is slightly different from removeTireFromQueryParam in lib/utils/strings
+  //   const param = key.replace(/tire/g, '').toLowerCase();
+  //   breadcrumbLabels[param] = activeFilters[key];
+  // });
+
+  // const breadcrumbs = mapPathnameToBreadcrumbs({
+  //   asPath,
+  //   pathname,
+  //   labels: breadcrumbLabels,
+  //   query,
+  // });
+
+  /*
+    TEMPORARY BREADCRUMBS
+    Replace this section with above comments when SEO landing pages for 
+    types/[type] and categories[category] are created
+   */
+  // There's only ever one section selected so the section key will be set once
   Object.keys(activeFilters).forEach((key) => {
-    const param = key.replace(/tire/g, '').toLowerCase();
-    breadcrumbLabels[param] = activeFilters[key];
+    // This util is slightly different from removeTireFromQueryParam in lib/utils/strings
+    // breadcrumbLabels['section'] = `${activeFilters[key]} reviews`;
+    breadcrumbLabels['section'] = ui('breadcrumbs.tireReviewsTemp', {
+      section: activeFilters[key],
+    });
   });
 
   const breadcrumbs = mapPathnameToBreadcrumbs({
     asPath,
-    pathname,
+    // Use pathname for /tire-reviews and temp path for brands/types/categories
+    pathname:
+      pathname === ROUTE_MAP[ROUTES.TIRE_REVIEWS]
+        ? pathname
+        : ROUTE_MAP[ROUTES.TIRE_REVIEWS_TEMP],
     labels: breadcrumbLabels,
     query,
   });
+  /*
+    End replace
+   */
 
   return {
     breadcrumbs,
