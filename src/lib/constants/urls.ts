469c1b436ef952511cf01f54f1018f4fba3e0ab9
Fernando Andrade <fernando.neto@gmail.com>
2020-08-12 16:19:43 -0400

    [WCS-1140] Replace ST API production URL

--- src/lib/constants/urls.ts
+++ src/lib/constants/urls.ts
@@ -5,8 +5,5 @@ export const URLS = {
   MAIN_API_INTEGRATION: 'https://master-services.jenkins.simpletire.com',
   MAIN_API_LOCAL: 'http://localhost:3000/api',
   MAIN_API_MOCK: 'https://steer-api-definition.now.sh/api',
-  // TODO: SWITCH TO PRODUCTION API
-  // https://simpletire.atlassian.net/browse/WCS-1140
-  // MAIN_API_PRODUCTION: 'https://services.simpletire.com',
-  MAIN_API_PRODUCTION: 'https://master-services.jenkins.simpletire.com',
+  MAIN_API_PRODUCTION: 'https://services.simpletire.com',
 };
