8b503f0e07c8226344a46146fe0db3211139957f
Gaston Figueroa <gastonfig@users.noreply.github.com>
2020-08-18 08:28:05 -0700

    [WCS-1531] Remove PDP shop w/ confidence link (#1151)

--- src/components/modules/PDP/ShopWithConfidence/ShopWithConfidence.tsx
+++ src/components/modules/PDP/ShopWithConfidence/ShopWithConfidence.tsx
@@ -3,9 +3,7 @@ import Grid from '~/components/global/Grid/Grid';
 import GridItem from '~/components/global/Grid/GridItem';
 import Icon from '~/components/global/Icon/Icon';
 import { ICONS } from '~/components/global/Icon/Icon.constants';
-import Link from '~/components/global/Link/Link';
 import Markdown from '~/components/global/Markdown/Markdown';
-import { THEME } from '~/lib/constants';
 import { ui } from '~/lib/utils/ui-dictionary';
 
 import { statsMock } from './ShopWithConfidence.mock';
@@ -20,16 +18,6 @@ function ShopWithConfidence() {
 
           <Icon name={ICONS.CHECKMARK} css={styles.checkmark} />
         </h2>
-
-        <Link
-          css={styles.learnMoreButton}
-          as="button"
-          // TODO update link url when available
-          href="/#"
-          theme={THEME.LIGHT}
-        >
-          {ui('pdp.shopWithConfidence.linkLabel')}
-        </Link>
       </GridItem>
 
       <GridItem fullbleed css={styles.carouselContainer}>
