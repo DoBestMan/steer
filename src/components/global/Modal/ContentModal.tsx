112c26fb7fb9437cc0f9924b235600600c31e57a
Alex Van Holtz <34043823+vanholtzatwork@users.noreply.github.com>
2020-08-21 08:19:21 -0700

    [WCS-1563] Close modal on link click (#1207)

--- src/components/global/Modal/ContentModal.tsx
+++ src/components/global/Modal/ContentModal.tsx
@@ -63,6 +63,7 @@ function ContentModal({
               isExternal={link.link.isExternal}
               theme={THEME.LIGHT}
               css={styles.link}
+              onClick={onClose}
             >
               {link.label}
             </Link>
