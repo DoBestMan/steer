871dcd1b7e22360de8c53734c00856acaac35c99
Arnaud Tanielian <arno.tanielian@gmail.com>
2020-08-25 10:53:25 -0400

    [Hotfix][v0.121.1] Feedbackify: Make sure the form displays on mobile (#1233)

--- src/styles/document/feedbackify.styles.ts
+++ src/styles/document/feedbackify.styles.ts
@@ -1,13 +1,15 @@
 export const feedbackify = `
     // TODO: Remove this temp file
     // Hide feedback floating button on S & M
-    #feedbackify {
-        display: none;
+    #feedbackify #fbya #fbyb .fby-tab,
+    #fby-tab-14912 {
+        display: none!important;
     }
 
     @media (min-width: 976px) {
-        #feedbackify {
-            display: block;
+        #feedbackify #fbya #fbyb .fby-tab,
+        #fby-tab-14912 {
+            display: block!important;
         }
     }
 `;
