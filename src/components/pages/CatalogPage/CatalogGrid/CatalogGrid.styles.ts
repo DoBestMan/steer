ae0e2610a1fb635d409a2d19154eec3a67f0e5fd
Alex Van Holtz <34043823+vanholtzatwork@users.noreply.github.com>
2020-08-31 07:58:35 -0700

    [WCS-1205] Fix catalog grid spacing regression (#1289)

--- src/components/pages/CatalogPage/CatalogGrid/CatalogGrid.styles.ts
+++ src/components/pages/CatalogPage/CatalogGrid/CatalogGrid.styles.ts
@@ -2,13 +2,9 @@ import { MQ, SPACING, StylesMap } from '~/lib/constants';
 
 const styles: StylesMap = {
   results: {
-    paddingBottom: SPACING.SIZE_60,
-
-    [MQ.M]: {
-      paddingBottom: SPACING.SIZE_50,
-    },
+    marginBottom: SPACING.SIZE_60,
     [MQ.L]: {
-      paddingBottom: 74,
+      marginBottom: SPACING.SIZE_80,
     },
   },
 };
