d57c40eb544f195640326dcc2c45735a06951c58
amish-waghmare <amish.waghmare@hnrtech.com>
2020-10-27 01:17:17 +0530

    FND-400: added press slug (#1416)
    * added press slug
    
    * FND-400: Updated slug setup for press pages
    
    * FND-400: Done updates
    
    * FND-400
    Done updates
    
    * redeploy
    
    * FND-400: Done updates
    
    * Added slugs for press subfolders

--- src/lib/constants/slugListBasePath.ts
+++ src/lib/constants/slugListBasePath.ts
@@ -1,4 +1,8 @@
 // export all the basePath urls defined in contentful for all SlugPages here
 export const slugListBasePath: Record<string, string> = {
+  pressCoverage: '/press/coverage',
+  pressInTheNews: '/press/in-the-news',
+  pressPage: '/press',
+  pressReleases: '/press/releases',
   sizeListingPage: '/tire-sizes/diameter',
 };
