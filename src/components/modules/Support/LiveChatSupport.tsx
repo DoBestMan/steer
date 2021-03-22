b6ae221d14d3227d822d56ba881d9f5f25aee74c
Vijay Trada <32730252+Vijay-Trada@users.noreply.github.com>
2021-03-23 01:18:53 +0530

    FND-602: added custom FS session (#1611)
    * FND-602: added custom FS session
    
    * FND-602: added custom FS session
    
    * FND-602: added custom FS session

--- src/components/modules/Support/LiveChatSupport.tsx
+++ src/components/modules/Support/LiveChatSupport.tsx
@@ -4,6 +4,7 @@ import Icon from '~/components/global/Icon/Icon';
 import { ICONS } from '~/components/global/Icon/Icon.constants';
 import { iconCTA } from '~/components/global/Link/Link.styles';
 import { appendLiveChatScript } from '~/lib/helpers/live-chat';
+import { isBrowser } from '~/lib/utils/browser';
 import { typography } from '~/styles/typography.styles';
 
 import styles from './LiveChatSupport.styles';
@@ -14,6 +15,9 @@ declare global {
       floatwindow: {
         visible: (visible: string) => void;
       };
+      visitor: {
+        info: (info: Record<string, string>) => void;
+      };
     };
   }
 }
@@ -21,6 +25,15 @@ declare global {
 function LiveChatSupport() {
   const [isScriptAppended, setIsScriptAppended] = useState<boolean>(false);
   const onClickHandler = () => {
+    if (
+      isBrowser() &&
+      window.FS &&
+      window.$zohosq &&
+      window.$zohosq?.floatwindow?.visible
+    ) {
+      const sessionUrl = window.FS.getCurrentSessionURL();
+      window.$zohosq.visitor.info({ fsSessionURL: sessionUrl });
+    }
     if (window.$zohosq && window.$zohosq?.floatwindow?.visible) {
       window.$zohosq.floatwindow.visible('show');
     }
