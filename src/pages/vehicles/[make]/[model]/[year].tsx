438972f3dcabbad4afefad5d61f0ee57ff4f37f5
bhavin-simpletire <56648054+bhavin-simpletire@users.noreply.github.com>
2021-03-31 21:04:13 +0530

    SQA-602: Set VehicleSearch Cookie Clientside (#1623)
    * SQA-602 : setVehicleSearch client Cookie
    
    * SQA-602 : setVehicleSearch client Cookie
    
    * SQA-602 : setVehicleSearch client Cookie
    
    * SQA-602 : setVehicleSearch client Cookie
    
    * SQA-602 : setVehicleSearch client Cookie
    
    * SQA-602 : setVehicleSearch client Cookie
    
    * SQA-602 : staging domain update
    
    * SQA-602 : lint fix
    
    * SQA-602: final changes
    
    * SQA-602 : final changes
    
    * SQA-602 : lint fix
    
    * SQA-602:Final changes
    
    * SQA-602:Final changes

--- src/pages/vehicles/[make]/[model]/[year].tsx
+++ src/pages/vehicles/[make]/[model]/[year].tsx
@@ -1,4 +1,7 @@
 import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { setCookie } from 'nookies';
+import { Cookies } from 'react-cookie';
 
 import CatalogPageContainer from '~/components/pages/CatalogPage/CatalogPage.container';
 import { CatalogPageData } from '~/components/pages/CatalogPage/CatalogPage.types';
@@ -12,8 +15,13 @@ import {
   backendGetVehicleProducts,
   backendGetVehicleSummary,
 } from '~/lib/backend/catalog/vehicle';
+import { ROUTE_MAP, ROUTES } from '~/lib/constants';
 import { getStringifiedParams } from '~/lib/utils/routes';
-import { removeTireFromQueryParam } from '~/lib/utils/string';
+import {
+  capitalize,
+  removeTireFromQueryParam,
+  unSlugify,
+} from '~/lib/utils/string';
 
 interface Props extends CatalogPageData {
   make: string;
@@ -31,6 +39,38 @@ function VehicleCatalog({ serverData, make, model, query, year }: Props) {
     trim: query && query.trim ? query.trim : undefined,
     year,
   };
+
+  const COOKIE_CONSTANT = {
+    COOKIE_NAME: 'vehicleCookie',
+    DOMAIN: 'localhost',
+  };
+  const router = useRouter();
+  if (router.pathname === ROUTE_MAP[ROUTES.VEHICLE_CATALOG]) {
+    const { year, make, model, trim } = router.query;
+    const formattedMake = unSlugify(removeTireFromQueryParam(make));
+    const formattedModel = unSlugify(model.toString());
+    const formattedTrim = capitalize(trim.toString());
+    const cookies = new Cookies();
+    const vehicleCookieValue = ` ${year},${formattedMake},${formattedModel},${formattedTrim} `;
+    if (
+      trim &&
+      typeof trim === 'string' &&
+      cookies.get(COOKIE_CONSTANT.COOKIE_NAME) !== vehicleCookieValue.toString()
+    ) {
+      // setting a Client side cookie for vehicleSearch.
+      setCookie(
+        null,
+        COOKIE_CONSTANT.COOKIE_NAME,
+        vehicleCookieValue.toString(),
+        {
+          maxAge: 86400 * 30,
+          path: '/',
+          secure: false,
+          domain: COOKIE_CONSTANT.DOMAIN,
+        },
+      );
+    }
+  }
 
   return (
     <CatalogPageContainer
