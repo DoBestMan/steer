504df29ca70f0293e7131bfa9c28f831dc89ea1d
zaktires <58437953+zaktires@users.noreply.github.com>
2021-05-03 19:57:34 +0530

    FND-627: Pass FullStory session url to Picquora API (#1659)
    Co-authored-by: Zak <mohammedzakkriya@yahoo.com>

--- src/components/modules/TireSnap/getTireSize.ts
+++ src/components/modules/TireSnap/getTireSize.ts
@@ -1,19 +1,26 @@
+import { isBrowser } from '~/lib/utils/browser';
+import { isProductionDeploy } from '~/lib/utils/deploy';
+
 export async function getTireSize(file: string, zipCode: string) {
   // Keeping it for now here, as this  will be moved to backend, once the service api is ready.
+  const prodUrl = 'https://simplesnap.picquora.com/api/v2/predict_tire_size';
+  const stagingUrl =
+    'https://simpletiredev.picquora.com/api/v2/predict_tire_size';
+  const picquoraApi = isProductionDeploy() ? prodUrl : stagingUrl;
   const formData = new FormData();
   formData.append('file', await b64toBlob(file), 'image.jpg');
   formData.append('api_key', 'bxzCh5MtzeXmApUZZ5kLxWrqKliQ1eC5ACK9pYvT');
   formData.append('client_id', '20');
   formData.append('zipcode', zipCode);
-
+  if (isBrowser() && window.FS) {
+    const sessionUrl = window.FS.getCurrentSessionURL();
+    formData.append('custom_field1', sessionUrl);
+  }
   try {
-    const res = await fetch(
-      'https://simpletiredev.picquora.com/api/v2/predict_tire_size',
-      {
-        body: formData,
-        method: 'post',
-      },
-    );
+    const res = await fetch(picquoraApi, {
+      body: formData,
+      method: 'post',
+    });
     return await res.json();
   } catch (e) {
     return Promise.reject(new Error(e));
